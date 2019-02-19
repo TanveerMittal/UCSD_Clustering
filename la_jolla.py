793474135b29fff8629c6d7648cd4ad59fb6b267
TanveerMittal <tanveer@mittals.us>
2019-02-19 13:12:58 -0800

    Refactoring

--- la_jolla.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-from visualize import draw_map
-
-df = pd.read_csv("places.csv")
-places = [[row[2], (row[3], row[4])] for row in df.itertuples()]
-draw_map(places)
